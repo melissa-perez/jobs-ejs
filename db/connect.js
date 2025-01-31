97149a367dd11f40edd467fcff0bcf0b1271593a
Melissa Perez <perezmelissa053@gmail.com>
2025-01-31 01:27:39 -0500

    Revert "Add up to boilerplate app.js"
    This reverts commit 43e382e6ad9b637ec35fc25a8f2155cc57e0e337.

--- db/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = (url) => {
-  return mongoose.connect(url).then(() => console.log("Connection to database established...")).catch((err) => {
-    console.log(err);
-  });
-};
-
-module.exports = connectDB;
