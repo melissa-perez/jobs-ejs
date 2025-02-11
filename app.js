7a67bb862a8478361a0859bb821f303cebb18fa6
Melissa Perez <perezmelissa053@gmail.com>
2025-02-11 00:06:36 -0500

    Finish lab

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(require("body-parser").urlencoded({ extended: true }));
 require("dotenv").config(); // to load the .env file into the process.env object
+
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const url = process.env.MONGO_URI;
@@ -33,47 +34,24 @@ if (app.get("env") === "production") {
 }
 
 app.use(session(sessionParms));
+
 const passport = require("passport");
 const passportInit = require("./passport/passportInit");
-
 passportInit();
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(require("connect-flash")());
-app.use(require("./middleware/storeLocals"));
 
+app.use(require("connect-flash")());
+
+app.use(require("./middleware/storeLocals"));
 app.get("/", (req, res) => {
     res.render("index");
 });
-
 app.use("/sessions", require("./routes/sessionRoutes"));
-// secret word handling
-//let secretWord = "syzygy";
-app.get("/secretWord", (req, res) => {
-    if (!req.session.secretWord) {
-        req.session.secretWord = "syzygy";
-    }
 
-    const errors = req.flash("error");
-    const info = req.flash("info");
-
-    res.render("secretWord", {
-        secretWord: req.session.secretWord,
-        errors,
-        info
-    });
-});
-
-app.post("/secretWord", (req, res) => {
-    if (req.body.secretWord.toUpperCase()[0] == "P") {
-        req.flash("error", "That word won't work!");
-        req.flash("error", "You can't use words that start with p.");
-    } else {
-        req.session.secretWord = req.body.secretWord;
-        req.flash("info", "The secret word was changed.");
-    }
-    res.redirect("/secretWord");
-});
+const secretWordRouter = require("./routes/secretWord");
+const auth = require("./middleware/auth");
+app.use("/secretWord", auth, secretWordRouter);
 
 app.use((req, res) => {
     res.status(404).send(`That page (${req.url}) was not found.`);
